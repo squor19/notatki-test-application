262b9586a202e0e95a634adc86b36ccfb268ae45
maksy <mak.martynenko@gmail.com>
2023-09-24 17:11:49 +0200

    working version included:
    -create, read, update, delete service implemented
    _All data cases are implemented

--- src/main/java/martynenko/test/notatki/entity/Notatka.java
+++ src/main/java/martynenko/test/notatki/entity/Notatka.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 
 @Entity
 public class Notatka {
+
     @Id
     @Column(name = "title")
     private String title;
@@ -16,4 +17,38 @@ public class Notatka {
     //Represents both a date and a time (yyyy-MM-dd-HH-mm-ss-ns)
     @Column(name = "last_updated")
     private LocalDateTime localDateAndTime;
+
+    public Notatka(String title, String content, LocalDateTime localDateAndTime) {
+        this.title = title;
+        this.content = content;
+        this.localDateAndTime = localDateAndTime;
+    }
+
+    public Notatka() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getLocalDateAndTime() {
+        return localDateAndTime;
+    }
+
+    public void setLocalDateAndTime(LocalDateTime localDateAndTime) {
+        this.localDateAndTime = localDateAndTime;
+    }
 }
--- src/main/java/martynenko/test/notatki/service/NotatkaService.java
+++ src/main/java/martynenko/test/notatki/service/NotatkaService.java
@@ -1,7 +1,11 @@
 package martynenko.test.notatki.service;
 
 import martynenko.test.notatki.entity.Notatka;
+import martynenko.test.notatki.repository.NotatkaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -9,23 +13,57 @@ public class NotatkaService {
 
     // CRUD
 
-    public void AddNotatka(Notatka notatka) {
+    private final NotatkaRepository repository;
 
+    @Autowired
+    public NotatkaService(NotatkaRepository repository) {
+        this.repository = repository;
+    }
+
+    public String addNotatka(Notatka notatka) {
+        String title = notatka.getTitle();
+        notatka.setLocalDateAndTime(LocalDateTime.now());
+        repository.save(notatka);
+        if (repository.findByTitle(title) != null) {
+            return "The note was successfully added!";
+        } else {
+            return "The note with that title already exists!";
+        }
     }
 
     public List<Notatka> findNotatkasByTitle(String title) {
-        return null;
+        return repository.findByTitleContaining(title);
     }
 
-    public void editNotatka(String title) {
-
+    public String updateNotatka(Notatka notatka) {
+        String title = notatka.getTitle();
+        if(repository.findByTitle(title) != null) {
+            repository.save(new Notatka(notatka.getTitle(), notatka.getContent(), LocalDateTime.now()));
+            if(repository.findByTitle(title).getContent().equals(notatka.getContent())) {
+                return "Note was successfully changed! ;)";
+            } else {
+                return "Failed to change the note ';(((";
+            }
+        } else {
+            return "Note with that title doesn't exist '_' ";
+        }
     }
 
-    public void deleteNotatka(String title) {
-
+    public String deleteNotatka(String title) {
+        Notatka temp = repository.findByTitle(title);
+        if (temp != null) {
+            repository.delete(temp);
+            if (repository.findByTitle(title) == null) {
+                return "Note was successfully removed";
+            } else {
+                return "Failed to delete the note";
+            }
+        } else {
+            return "Note with that title doesn't exist";
+        }
     }
 
     public List<Notatka> showAllNotatkas() {
-        return null;
+        return repository.findAll();
     }
 }
